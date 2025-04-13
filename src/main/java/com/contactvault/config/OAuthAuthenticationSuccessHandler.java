94f4d9d1a636d91c405ff63ad667e886f93bbf0a
rohitbhaskaruday <rohitbaskar26@gmail.com>
2025-04-13 14:19:21 -0700

    Added Git login and Google Login

---
+++ src/main/java/com/contactvault/config/OAuthAuthenticationSuccessHandler.java
@@ -0,0 +1,34 @@
+package com.contactvault.config;
+
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.interceptor.LoggingCacheErrorHandler;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class OAuthAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+
+    Logger logger = LoggerFactory.getLogger(OAuthAuthenticationSuccessHandler.class);
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
+        logger.info("OAuthAuthenticationSuccessHandler");
+
+        new DefaultRedirectStrategy().sendRedirect(request, response, null);
+    }
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
+    }
+}
